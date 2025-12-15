1842ab39251b33716ffb018d54ad195a78fa9e08
nguyuendang3942-byte <nguyuendang3942@gmail.com>
2025-12-15 21:30:44 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,9 @@
+local gui = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
+
+local label = Instance.new("TextLabel", gui)
+label.Size = UDim2.fromScale(0.4, 0.1)
+label.Position = UDim2.fromScale(0.3, 0.45)
+label.Text = "LOADSTRING OK"
+label.TextScaled = true
+label.BackgroundColor3 = Color3.fromRGB(0, 170, 0)
+label.TextColor3 = Color3.new(1,1,1)
